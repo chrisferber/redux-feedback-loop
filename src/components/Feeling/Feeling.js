0834a1ff36fa8113b42a26de8f06e869ee410818
Chris Ferber <chris@ferbers.us>
2019-11-03 17:49:29 -0600

    fix route validation from Feeling to Understanding component

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { HashRouter as Router, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class Feeling extends Component {
 
     state = {
+        toUnderstanding: false,
         submission: {
             question: 'feeling',
             rating: '',
@@ -23,27 +24,30 @@ class Feeling extends Component {
 
     handleNextButtonClick = () => {
         if (this.state.submission.rating === '' || this.state.submission.rating < 0 || this.state.submission.rating > 5) {
-            alert('Invalid input, please enter a number 1-5');
+            alert('Invalid input, please enter a number 0-5');
         } else {
             this.props.dispatch({ type: 'FEELING_RATING', payload: this.state.submission });
-            return <Link to="/understanding"></Link>;
+            this.setState({toUnderstanding: true});
         }
     }
 
     render() {
+
+        if (this.state.toUnderstanding === true) {
+            return <Redirect to='/understanding' />
+        }
+
         return (
             <>
                 <div className="Feeling">
                     <p>How are you feeling today?</p>
                     <input onChange={this.catchInput} type="number" placeholder="rating" value={this.state.rating} />
                 </div>
-                <Router>
                     <div>
                         <button onClick={this.handleNextButtonClick} >
                             Next
                         </button>
                     </div>
-                </Router>
             </>
         );
     }
--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { HashRouter as Router, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 
 class Understanding extends Component {
+
+    state = {
+        toSupported: false,
+        submission: {
+            question: 'feeling',
+            rating: '',
+        }
+    }
+
+    catchInput = (event) => {
+        this.setState({
+            submission: {
+                ...this.state.submission,
+                rating: event.target.value,
+            }
+        })
+    }
+
+    handleNextButtonClick = () => {
+        if (this.state.submission.rating === '' || this.state.submission.rating < 0 || this.state.submission.rating > 5) {
+            alert('Invalid input, please enter a number 0-5');
+        } else {
+            this.props.dispatch({ type: 'FEELING_RATING', payload: this.state.submission });
+            this.setState({toSupported: true});
+        }
+    }
+
     render() {
+
+        if (this.state.toSupported === true) {
+            return <Redirect to='/supported' />
+        }
+
         return (
             <>
-                <div className="Understanding">
-                    <h1>Understanding!</h1>
+            <div className="Understanding">
+                <p>How are you feeling today?</p>
+                <input onChange={this.catchInput} type="number" placeholder="rating (0-5)" value={this.state.rating} />
+            </div>
+                <div>
+                    <button onClick={this.handleNextButtonClick} >
+                        Next
+                    </button>
                 </div>
-                <Router>
-                    <div>
-                        <button>
-                            <Link to="/supported">Next</Link>
-                        </button>
-                    </div>
-                </Router>
-            </>
+        </>
         );
     }
 }
